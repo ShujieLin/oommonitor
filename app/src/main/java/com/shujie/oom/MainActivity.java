8835871105f011f846ee9e22e4036d8d647e8de2
LinShuJie(林树杰/深圳) <linshujie@paxsz.local>
2024-03-19 17:06:42 +0800

    Initial commit： add the base code from koom

---
+++ app/src/main/java/com/shujie/oom/MainActivity.java
@@ -0,0 +1,34 @@
+package com.shujie.oom;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import com.shujie.oom.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'oom' library on application startup.
+    static {
+        System.loadLibrary("oom");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'oom' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
